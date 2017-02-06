9c49051ba4cb546fc8cc7674fadc91aca46beba4
jaejoon <kimjaejoon@kimjaejoonui-MacBook-Pro.local>
2017-02-06 18:47:15 +0900

    Add Python code in html file to show some result

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey ('auth.user')
     title = models.CharField (max_length=200)
     text = models.TextField(max_length=500,blank=True,null=True)
-    created_date = models.DateTimeField() # default=timezone.now
-    published_date = models.DateTimeField() # blank=True, null=True
+    created_date = models.DateTimeField(default=timezone.now) # default=timezone.now
+    published_date = models.DateTimeField(blank=True, null=True) # blank=True, null=True
 
     def publish(self):
         self.published_date = timezone.now
